b3229d892888e0bd992ca4915c703c99a165865b
jkremes <only1truth333@gmail.com>
2018-12-04 13:20:37 -0500

    fixing router issues

--- app/models/user.js
+++ app/models/user.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
   email: {
     type: String,
     required: true,
@@ -8,6 +12,10 @@ const userSchema = new mongoose.Schema({
   },
   hashedPassword: {
     type: String,
+    required: true
+  },
+  is_admin: {
+    type: Boolean,
     required: true
   },
   token: String
